be453a3cfcb6a7bb92ce12c5d92eeaa007478d45
SkayBlack <adderchavajay7@gmail.com>
2021-10-12 10:09:23 -0600

    boton crear cuenta agregado en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,12 @@
 @section('title','Login')
 
 @section('main')
-    
+
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">                                       
-                <img src="{{asset('images/icons/icons/Logo Mercado.ico')}}" alt="" class=" w-40 h-40" />                 
+            <a href="/">
+                <img src="{{asset('images/icons/icons/Logo Mercado.ico')}}" alt="" class=" w-40 h-40" />
             </a>
         </x-slot>
 
@@ -25,11 +25,12 @@
                 <x-label for="email" :value="__('Correo electronico')" />
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                      <span class="input-group-text" id="basic-addon1">@</span>
+                        <span class="input-group-text" id="basic-addon1">@</span>
                     </div>
-                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus placeholder="email@ejemplo.com" />
-                  </div>
-               
+                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required
+                        autofocus placeholder="email@ejemplo.com" />
+                </div>
+
             </div>
 
             <!-- Password -->
@@ -38,25 +39,32 @@
 
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                      <span class="input-group-text" id="basic-addon1"><ion-icon name="key-outline"></ion-icon></span>
+                        <span class="input-group-text" id="basic-addon1">
+                            <ion-icon name="key-outline"></ion-icon>
+                        </span>
                     </div>
-                    <x-input id="password" class="form-control" type="password" name="password"  required autocomplete="current-password" />
-                  </div>
+                    <x-input id="password" class="form-control" type="password" name="password" required
+                        autocomplete="current-password" />
+                </div>
             </div>
 
             <div class="flex items-center justify-center mt-4">
                 {{-- @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif --}}
 
-                <button class="btn btn-dark">
+                <button class="btn btn-dark mx-3">
                     {{ __('Iniciar sesion') }}
                 </button>
+
+                <a href="{{route('register')}}" class="btn btn-dark mx-3">
+                    crear cuenta
+                </a>
             </div>
         </form>
     </x-auth-card>
 </x-guest-layout>
-    
+
 @endsection
