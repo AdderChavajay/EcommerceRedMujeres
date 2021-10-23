98c8e026d67b3fe2e0412a35ba63ef12a1c15d2c
SkayBlack <adderchavajay7@gmail.com>
2021-10-22 19:18:59 -0600

    tabla detalles del producto en show, SoldProduct

---
+++ resources/views/SoldProduct/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('main')
+<div class="Card container">
+    <div class="card-header text-center">
+        <h5>Registro de Usuarios</h5>
+    </div>
+    <div class="card-body overflow-auto">
+        <table class="table table-hover">
+            <thead class="thead-dark">
+                <tr class="">
+                    <th scope=""># factura</th>
+                    <th scope="">Producto</th>
+                    <th scope="">Cantidad</th>
+                    <th scope="">Subtotal</th>
+                    <th scope="">Catidad Producto</th>
+                    <th scope="">Catidad producto</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr class="">
+                    <th class="">id</th>
+                    <th class="">nombre</th>
+                    <td class="">21</td>
+                    <td class="">$3</td>
+                    <td class="">4</td>
+
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
