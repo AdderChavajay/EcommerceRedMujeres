99d89294745c6947eac6c055fcc1e6563e7e7ed5
Miguel Xet <mikevi77@gmail.com>
2021-10-22 20:59:02 -0600

    Fix: image for success purchased

--- resources/views/shopping/success.blade.php
+++ resources/views/shopping/success.blade.php
@@ -8,12 +8,13 @@
 <main>
     <div class="mx-auto s row align-item-center justify-content-center vh-100">
         <div class="mx-auto align-self-center">
-            <div class="d-flex justify-content-center">
-                <img src="{{asset('images/check.png')}}" alt="" width="100rem">
+            <div class="d-flex justify-content-center" style="width: 200px;">
+                <img src="{{asset('images/icons/check.png')}}" alt="" width="100px">
             </div>
             <div class="py-3">
                 <h3>Compra Realizada</h3>
                 <p>Esperamos que vuelva pronto...</p>
+                <a href="{{ route('main') }}" class="btn btn-primary">Seguir comprando</a>
             </div>
         </div>
     </div>
